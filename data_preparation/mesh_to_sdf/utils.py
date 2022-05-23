541a8f5be9ebb04e02cfa7079db11e8c22f049a5
sophiajwagner <wagsoph@gmail.com>
2022-05-23 17:59:56 +0200

    added code for data preparation

---
+++ data_preparation/mesh_to_sdf/utils.py
@@ -0,0 +1,54 @@
+import functools
+import trimesh
+import numpy as np
+
+def scale_to_unit_sphere(mesh):
+    if isinstance(mesh, trimesh.Scene):
+        mesh = mesh.dump().sum()
+
+    vertices = mesh.vertices - mesh.bounding_box.centroid
+    distances = np.linalg.norm(vertices, axis=1)
+    vertices /= np.max(distances)
+
+    return trimesh.Trimesh(vertices=vertices, faces=mesh.faces)
+
+def scale_to_unit_cube(mesh):
+    if isinstance(mesh, trimesh.Scene):
+        mesh = mesh.dump().sum()
+
+    vertices = mesh.vertices - mesh.bounding_box.centroid
+    vertices *= 2 / np.max(mesh.bounding_box.extents)
+
+    return trimesh.Trimesh(vertices=vertices, faces=mesh.faces)
+
+
+# Use get_raster_points.cache_clear() to clear the cache
+@functools.lru_cache(maxsize=4)
+def get_raster_points(voxel_resolution):
+    points = np.meshgrid(
+        np.linspace(-1, 1, voxel_resolution),
+        np.linspace(-1, 1, voxel_resolution),
+        np.linspace(-1, 1, voxel_resolution)
+    )
+    points = np.stack(points)
+    points = np.swapaxes(points, 1, 2)
+    points = points.reshape(3, -1).transpose().astype(np.float32)
+    return points
+
+
+def check_voxels(voxels):
+    block = voxels[:-1, :-1, :-1]
+    d1 = (block - voxels[1:, :-1, :-1]).reshape(-1)
+    d2 = (block - voxels[:-1, 1:, :-1]).reshape(-1)
+    d3 = (block - voxels[:-1, :-1, 1:]).reshape(-1)
+
+    max_distance = max(np.max(d1), np.max(d2), np.max(d3))
+    return max_distance < 2.0 / voxels.shape[0] * 3**0.5 * 1.1
+
+
+def sample_uniform_points(amount):
+    '''changed to box instead of sphere'''
+    uniform_points = np.random.uniform(-1, 1, size=(amount * 2 + 20, 3))
+    return uniform_points
+
+
---
+++ data_preparation/prepare_data.py
@@ -0,0 +1,30 @@
+import os
+import trimesh
+from mesh_to_sdf import get_surface_point_cloud
+from pyntcloud import PyntCloud
+import pandas as pd
+
+#takes way too long
+def prepare_data():
+    data_path = os.path.join('example','shapenet')
+    output_dir = os.path.join('example','shapenet_prep')
+    sample_point_count = 100000 #config
+    query_point_count = 100000 #config
+    i = 0
+    for dir in os.listdir(data_path):
+        file_path = os.path.join(*[data_path,dir,'models','model_normalized.obj'])
+        mesh = trimesh.load(file_path)
+        #maybe change to scan later
+        surface_pc = get_surface_point_cloud(mesh, surface_point_method='sample', sample_point_count=sample_point_count)
+        pc_points = surface_pc.points
+        query_points, sdf = surface_pc.sample_sdf_near_surface(number_of_points=query_point_count)
+
+        #save
+        sub_output_dir = os.path.join(output_dir,str(i))
+        os.makedirs(sub_output_dir)
+        point_cloud = PyntCloud(pd.DataFrame(data={'x': pc_points[:,0],'y': pc_points[:,1],'z': pc_points[:,2]}))
+        point_cloud.to_file(os.path.join(sub_output_dir,'points.ply'))
+        query_point_cloud = PyntCloud(pd.DataFrame(data={'x': query_points[:,0],'y': query_points[:,1],'z': query_points[:,2], 'sdf': sdf}))
+        query_point_cloud.to_file(os.path.join(sub_output_dir,'query_points.ply'))
+        #os.rmdir(dir)
+        i += 1
