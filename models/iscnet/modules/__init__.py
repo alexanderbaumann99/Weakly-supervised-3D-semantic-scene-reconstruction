edb6d6c79094366fb8195c8d83947c0f65f60969
alexanderbaumann99 <alex.baumi@freenet.de>
2022-06-20 22:24:16 +0200

    Fixed initialization of modules

--- models/iscnet/modules/__init__.py
+++ models/iscnet/modules/__init__.py
@@ -4,5 +4,7 @@ from .proposal_module import ProposalModule
 from .vote_module import VotingModule
 from .occupancy_net import ONet
 from .skip_propagation import SkipPropagation
+from .group_and_align import GroupAndAlign
+from .shape_prior import ShapePrior
 
-__all__ = ['ISCNet', 'Pointnet2Backbone', 'ProposalModule', 'VotingModule', 'ONet', 'SkipPropagation']
+__all__ = ['ISCNet','GroupAndAlign','Pointnet2Backbone', 'ProposalModule', 'VotingModule', 'ONet', 'SkipPropagation','ShapePrior']
