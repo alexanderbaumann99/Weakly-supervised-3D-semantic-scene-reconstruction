e802b28f07b8d3036e7431f7a27788a106c69584
sophiajwagner <wagsoph@gmail.com>
2022-06-23 15:25:17 +0200

    added test generator prior

--- visuals_mesh_reconstruction.py
+++ visuals_mesh_reconstruction.py
@@ -1,15 +1,15 @@
 import open3d as o3d
 import numpy as np
 
-n=233
+#n=233
 
-pcd = o3d.io.read_point_cloud("gt"+str(n)+".pcd")
-o3d.visualization.draw_geometries([pcd])
+#pcd = o3d.io.read_point_cloud("gt"+str(n)+".pcd")
+#o3d.visualization.draw_geometries([pcd])
 
-pcd2 = o3d.io.read_point_cloud("input"+str(n)+".pcd")
-o3d.visualization.draw_geometries([pcd2])
+#pcd2 = o3d.io.read_point_cloud("input"+str(n)+".pcd")
+#o3d.visualization.draw_geometries([pcd2])
 
-mesh = o3d.io.read_triangle_mesh("item"+str(n)+".ply")
+mesh = o3d.io.read_triangle_mesh("out/prior_meshes/test_generator_prior.ply")
 o3d.visualization.draw_geometries([mesh])
 
 
