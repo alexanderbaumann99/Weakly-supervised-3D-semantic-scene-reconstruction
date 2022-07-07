fc17e21a81ae2e0692ac0f5d3978407ecd3bd5c9
Alexander Baumann <88405797+alexanderbaumann99@users.noreply.github.com>
2022-07-07 18:06:21 +0200

    Update prepare_data.py

--- data_preparation/prepare_data.py
+++ data_preparation/prepare_data.py
@@ -1,22 +1,21 @@
-import imp
 import os
 import trimesh
 from mesh_to_sdf import get_surface_point_cloud
 from pyntcloud import PyntCloud
 import pandas as pd
 import numpy as np
-import math
+import argparse 
+import yaml
 
 ShapeNet2Labels = {'04379243':'0','03001627':'1','02871439':'2','04256520':'3','02747177':'4','02933112':'5','03211117':'6','02808440':'7' }
 
-
-
-def prepare_data(ids=True):
-    data_path = '../raw/shapenet/watertight_scaled_simplified'
-    output_dir = '../datasets/ShapeNetv2_data_fps2'
-    sample_point_count = 50000 #config, only for testing
-    point_cloud_count = 2048
-    query_point_count = 50000 #config
+def prepare_data(cfg,input_path):
+    data_path = input_path
+    output_dir = cfg['data']['shapenet_path']
+    point_cloud_count  = cfg['data']['num_box_points']
+    query_point_count  = cfg['data']['prep_num_query_points'] 
+    
+    sample_point_count = 50000
     i = 0
     for root, dirs, files in os.walk(data_path):
         for file in files:
@@ -38,21 +37,27 @@ def prepare_data(ids=True):
             query_point_cloud_sphere = PyntCloud(pd.DataFrame(data={'x': query_points_sphere[:,0],'y': query_points_sphere[:,1],'z': query_points_sphere[:,2], 'sdf': sdf_sphere}))
             query_point_cloud_sphere.to_file(os.path.join(sub_output_dir,'query_points_sphere.ply'))
 
-            if ids:
-                dic={}
-                id = root.split('/')[-1] 
-                dic['shapenet_id'] = id
-                dic['sem_cls_id']  = ShapeNet2Labels[id] 
+        
+            dic={}
+            id = root.split('/')[-1] 
+            dic['shapenet_id'] = id
+            dic['sem_cls_id']  = ShapeNet2Labels[id] 
 
-                #save
-                np.save(os.path.join(sub_output_dir,'dict_ids'),dic)
-
-            
-            
+            #save
+            np.save(os.path.join(sub_output_dir,'dict_ids'),dic)
+          
             i += 1
 
 
 if __name__ == '__main__':
-    prepare_data()
+    parser = argparse.ArgumentParser('data preparation')
+    parser.add_argument('--config', type=str, default='../configs/config_files/ISCNet.yaml',
+                        help='configure file')
+    parser.add_argument('--input-path', type=str, help='path for raw ShapeNet data')
+    args = parser.parse_args()
+    
+    with open(args.config, 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    prepare_data(cfg,args.input_path)
 
 
